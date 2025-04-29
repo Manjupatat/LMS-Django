2e2d129d4f0ce8d34e60bee630996d1b54913e84
Manjunath L Patat <109718621+Manjupatat@users.noreply.github.com>
2025-04-29 20:38:56 +0530

    Update settings.py

--- lms_project/settings.py
+++ lms_project/settings.py
@@ -31,7 +31,9 @@ SECRET_KEY = 'django-insecure-&0v7ho0#qz!uxl%09t1ryz9b9+5r8@wsl8bksfn+=mm$io*rx!
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
+CSRF_TRUSTED_ORIGINS = [
+    "https://lms-django-c6gn.onrender.com"
+]
 
 # Application definition
 
