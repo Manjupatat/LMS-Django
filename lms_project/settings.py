b3b376b6da1efa56361c3d2dd0dfdd2d929f8461
Manjunath L Patat <109718621+Manjupatat@users.noreply.github.com>
2025-05-04 17:13:10 +0530

    Update settings.py

--- lms_project/settings.py
+++ lms_project/settings.py
@@ -28,7 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&0v7ho0#qz!uxl%09t1ryz9b9+5r8@wsl8bksfn+=mm$io*rx!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+TEMPLATE_DEBUG=False
 
 ALLOWED_HOSTS = ['*']
 # ALLOWED_HOSTS = ['lms-django-c6gn.onrender.com']
